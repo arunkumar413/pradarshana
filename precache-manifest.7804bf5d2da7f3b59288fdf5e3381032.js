ddf5354851a4629f3702dee15c392b0dddf92982
arunkumar413 <arunkumar413@gmail.com>
2022-10-04 11:59:53 +0530

    Updates

---
+++ precache-manifest.7804bf5d2da7f3b59288fdf5e3381032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efa7347fa0181c54d32456da15962a3",
+    "url": "/pradarshana/index.html"
+  },
+  {
+    "revision": "35a307479b993b28fa16",
+    "url": "/pradarshana/static/css/2.aa61572a.chunk.css"
+  },
+  {
+    "revision": "09cf82c78421d3a2c316",
+    "url": "/pradarshana/static/css/main.fe057dd1.chunk.css"
+  },
+  {
+    "revision": "35a307479b993b28fa16",
+    "url": "/pradarshana/static/js/2.9710897c.chunk.js"
+  },
+  {
+    "revision": "3caaeab42b48d0c867550bcf1f898c6a",
+    "url": "/pradarshana/static/js/2.9710897c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cf82c78421d3a2c316",
+    "url": "/pradarshana/static/js/main.c71aca1b.chunk.js"
+  },
+  {
+    "revision": "172912dff0dd391a8a01",
+    "url": "/pradarshana/static/js/runtime-main.533e8647.js"
+  }
+]);
